19fc9888daa843f8af8de726dbb6d7d4d215d894
Anirudh Pupneja <apupneja2002@gmail.com>
2023-09-12 13:04:18 -0400

    server side debug

--- exec.sh
+++ exec.sh
@@ -1,1 +1,1 @@
-python fairseq_cli/hydra_train.py --config-dir /home/zyu401_shadow/fairseq-quant/examples/wav2vec/config/finetuning/ --config-name base_100h
+python fairseq_cli/hydra_train.py --config-dir /home/zyu401_shadow/fairseq-quant/examples/wav2vec/config/finetuning/ --config-name base_100h  common.tensorboard_logdir=/home/zyu401_shadow/fairseq-quant/logs
---
+++ exec_not_quant.sh
@@ -0,0 +1,1 @@
+python fairseq_cli/hydra_train.py --config-dir /home/zyu401_shadow/fairseq-quant/examples/wav2vec/config/finetuning/ --config-name base_100h_notquant  common.tensorboard_logdir=/home/zyu401_shadow/fairseq-quant/logs
